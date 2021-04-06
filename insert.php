550c61da590bd82866ee1ecbc4dc55e3266fbf76
siamshaeed <siamshaeed@gmail.com>
2021-04-06 22:48:10 +0600

    database connect

--- insert.php
+++ insert.php
@@ -1,5 +1,6 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
+$db_connect = new mysqli("localhost", "root", '', "php-crud");
 
- ?>
+?>
