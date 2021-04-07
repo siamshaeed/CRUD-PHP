981e803ef2f929c8ea93972f47e06d6fc0bba60d
siamshaeed <siamshaeed@gmail.com>
2021-04-07 08:37:24 +0600

    html basic structure

--- insert.php
+++ insert.php
@@ -2,5 +2,15 @@
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $db_connect = new mysqli("localhost", "root", '', "php-crud");
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
