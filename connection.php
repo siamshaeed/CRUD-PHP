3cf4fd2f7407dfd34dc31c961d4191e0c41f039b
siamshaeed <siamshaeed@gmail.com>
2021-04-14 16:04:48 +0600

    Database connection

--- connection.php
+++ connection.php
@@ -1,8 +1,15 @@
-<!-- database connection way : 1 -->
+<!-- Database connection way : 1 -->
 <?php 
 $host = "localhost";
 $user = "root";
 $pass = "";
 $database = "php-crud";
-$db = mysqli_connect("$host", "$user", "$pass", "$database") or die('no database');
- ?>
+//Connection with mysql server
+$conn = mysqli_connect("$host", "$user", "$pass", "$database") or die('Error Database Connection');
+//chack database connection
+if ($conn) {
+	echo "Database Connection Success";
+}else{
+	echo "Database connection loss";
+}
+?>
