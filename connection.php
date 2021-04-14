1988f0d88a7033aeaeb088e96211a60095a0ae14
siamshaeed <siamshaeed@gmail.com>
2021-04-14 14:50:47 +0600

    database connect way 1

--- connection.php
+++ connection.php
@@ -1,17 +1,8 @@
-<!-- Database connection way : 1 --> 
-<?php
+<!-- database connection way : 1 -->
+<?php 
 $host = "localhost";
 $user = "root";
 $pass = "";
-$database = "crudsample";
-//Connection with mysql server
-$conn = mysqli_connect($host, $user, $pass, $database) or die ("Something wrong. Please connet with your Server Administrator ");
-
-//chack database connection
-if($conn){
-	echo "Connection Success";
-}else{
-	echo"Connection Problem";
-}
-
-?>
+$database = "php-crud";
+$db = mysqli_connect("$host", "$user", "$pass", "$database") or die('no database');
+ ?>
--- create.php
+++ create.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$db = new mysqli("localhost", "root", '', "php-crud");
+// $db = new mysqli("localhost", "root", '', "php-crud");
 ?>
 <!DOCTYPE html>
 <html lang="en">
