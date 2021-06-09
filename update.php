0d9e2cc769ba72c8828c4f7b437498ddc9d61b5b
siamshaeed <siamshaeed@gmail.com>
2021-06-09 23:05:04 +0600

    blank

--- update.php
+++ update.php
@@ -54,7 +54,7 @@ $db_connect = new mysqli("localhost", "root", '', "php-crud");
 		<div class="row">
 			<div class="col-md-12">
 				<div class="myBody">
-					<p class="bodytext test">Update page</p>
+					<p class="bodytext test">Update Page</p>
 				</div>
 			</div>
 		</div>
